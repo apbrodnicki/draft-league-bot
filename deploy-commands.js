110e4c09731d6b0567f2b9190a8016aa7a4657de
Alex Brodnicki <apbrodnicki@gmail.com>
2024-02-07 10:11:36 -0600

    Move redundant code into a helper file.

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,24 +1,8 @@
 const { REST, Routes } = require('discord.js');
 const { clientId, token } = require('./config.json');
-const fs = require('node:fs');
-const path = require('node:path');
+const { addCommands } = require('./helper');
 
-const commands = [];
-
-const foldersPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(foldersPath);
-
-for (const file of commandFiles) {
-	const filePath = path.join(foldersPath, file);
-	const command = require(filePath);
-
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		commands.push(command.data.toJSON());
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-}
+const commands = addCommands([]);
 
 // Construct and prepare an instance of the REST module
 const rest = new REST().setToken(token);
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const fs = require('node:fs');
-const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const { addCommands } = require('./helper');
 
 // Create a new client instance
 const client = new Client({
@@ -13,22 +12,7 @@ const client = new Client({
 	],
 });
 
-client.commands = new Collection();
-
-const foldersPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(foldersPath);
-
-for (const file of commandFiles) {
-	const filePath = path.join(foldersPath, file);
-	const command = require(filePath);
-
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-}
+client.commands = addCommands(new Collection());
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
